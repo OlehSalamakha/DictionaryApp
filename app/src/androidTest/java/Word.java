635a835347a29487eadee8d155e07a8346bd4b9e
OlehSalamakha <oleh.salamakha@globallogic.com>
2015-01-13 15:40:44 +0200

    Word class

---
+++ app/src/androidTest/java/Word.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+/**
+ * Created by olehsalamakha on 1/13/15.
+ */
+public class Word {
+
+	public Word(String word, List<String> translations) {
+		word_ = word;
+		translations_ = translations;
+	}
+
+
+	public String getWord() {
+		return word_;
+	}
+
+	public List<String> getTranslations() {
+		return translations_;
+	}
+
+	private String word_;
+	private List<String> translations_;
+}
